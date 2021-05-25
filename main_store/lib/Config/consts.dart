12886422824f412c4cac4ab52ba2c2703b15962c
Asfand Yr <asfandyr@Asfands-MacBook-Pro.local>
2021-05-25 18:33:10 +0500

    Normal Changes in file

---
+++ main_store/lib/Config/consts.dart
@@ -0,0 +1,10 @@
+// Themes
+import 'package:flutter/material.dart';
+
+final Color accentColor = Colors.green;
+final footerColor = Color(0xffebebeb);
+// Strings
+final String agreeing_term_text =
+    'By Creating an Account You agree to \nour Free Membership Agreement and Privacy Policy';
+final String footer_desc =
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. \nLorem Ipsum has been the industry's standard dummy text ever since the 1500s, \nwhen an unknown printer took a galley of type and scrambled \nit to make a type specimen book.";
--- main_store/lib/View/Sign_up/signup_view.dart
+++ main_store/lib/View/Sign_up/signup_view.dart
@@ -1,16 +1,118 @@
 import 'package:flutter/material.dart';
+import 'package:main_store/Config/consts.dart';
+import 'package:main_store/Config/sizeconfig.dart';
+import 'package:main_store/View/Componants/Forum.dart';
 import 'package:main_store/View/Sign_up/signup_viewmodel.dart';
 import 'package:stacked/stacked.dart';
 
 class SignupPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SizeConfig().init(context);
     return ViewModelBuilder<SignupViewModel>.reactive(
       viewModelBuilder: () => SignupViewModel(),
       builder: (context, model, child) => Scaffold(
-        body: Container(
-          child: Text('Sign Up Page'),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Center(child: Forum()),
+            Footer(),
+          ],
         ),
+      ),
+    );
+  }
+}
+
+class Footer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: SizeConfig.blockSizeVertical * 25,
+      decoration: BoxDecoration(color: footerColor),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              // Logo Image
+              Container(
+                height: 40,
+              ),
+              Container(
+                child: Text(footer_desc, style: TextStyle(fontSize: 10)),
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 6),
+            child: VerticalDivider(
+              color: Colors.grey,
+            ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                padding: EdgeInsets.only(top: 20),
+                child: Text(
+                  'Quick Links',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w300),
+                ),
+              ),
+              Container(
+                child: Text(
+                  'Home',
+                  style: TextStyle(fontSize: 15),
+                ),
+              ),
+              Container(
+                child: Text(
+                  'About',
+                  style: TextStyle(fontSize: 15),
+                ),
+              ),
+              Container(
+                child: Text(
+                  'Contact',
+                  style: TextStyle(fontSize: 15),
+                ),
+              ),
+              Container(
+                child: Text(
+                  'Privacy Policy',
+                  style: TextStyle(fontSize: 15),
+                ),
+              ),
+              Container(
+                child: Text(
+                  'Terms & Condition',
+                  style: TextStyle(fontSize: 15),
+                ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 6),
+            child: VerticalDivider(
+              color: Colors.grey,
+            ),
+          ),
+          Column(
+            children: [
+              Container(
+                padding: EdgeInsets.only(top: 10),
+                child: Text(
+                  'Download App',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w300),
+                ),
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
