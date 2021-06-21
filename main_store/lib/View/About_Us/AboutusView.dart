4d8cd33351b13308c450a402dcfb32f918ebe3e6
Muhammad Zia Ul Qamar <devzia@MacBook-Air.local>
2021-06-21 19:35:23 +0500

    About Page Created

--- main_store/lib/View/About_Us/AboutusView.dart
+++ main_store/lib/View/About_Us/AboutusView.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:main_store/Config/consts.dart';
+import 'package:main_store/Config/sizeconfig.dart';
 import 'package:main_store/View/About_Us/AboutViewModel.dart';
 import 'package:main_store/View/Componants/Footer/FooterView.dart';
 import 'package:main_store/View/Componants/Header/Header.dart';
@@ -9,27 +11,35 @@ class AboutView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SizeConfig().init(context);
     return ViewModelBuilder.reactive(
       viewModelBuilder: () => AboutViewModel(),
       builder: (context, model, child) => Scaffold(
         body: SingleChildScrollView(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               // Header
               Container(
-                decoration: BoxDecoration(color: Colors.white, boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.5),
-                    blurRadius: 2,
-                  )
-                ]),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      blurRadius: 2,
+                    ),
+                  ],
+                ),
                 child: Header(
                   isSignInPage: true,
                 ),
               ),
-              // About Us Page
-
-              // Footer
+              // // About Us Page
+              AboutUsDetails(),
+              SizedBox(
+                height: SizeConfig.blockSizeVertical * 3,
+              ),
+              // Footer,
               Container(
                 child: Footer(),
               )
@@ -37,6 +47,73 @@ class AboutView extends StatelessWidget {
           ),
         ),
       ),
+    );
+  }
+}
+
+class AboutUsDetails extends StatelessWidget {
+  const AboutUsDetails({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          alignment: Alignment.center,
+          child: Padding(
+            padding: EdgeInsets.only(top: SizeConfig.blockSizeVertical * 3),
+            child: Text(
+              'About Us',
+              style: TextStyle(
+                fontSize: 36,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(top: SizeConfig.blockSizeVertical * 5),
+              child: Container(
+                height: SizeConfig.blockSizeVertical * 30,
+                width: SizeConfig.blockSizeVertical * 30,
+                alignment: Alignment.centerLeft,
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(blurRadius: 4, color: Colors.grey.shade500),
+                  ],
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/grocerystore.png'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                left: SizeConfig.blockSizeHorizontal * 3,
+                top: SizeConfig.blockSizeVertical * 5,
+              ),
+              child: Container(
+                width: SizeConfig.blockSizeHorizontal * 45,
+                child: Text(
+                  '$aboutUSText',
+                  softWrap: true,
+                  textAlign: TextAlign.justify,
+                  style: TextStyle(
+                    fontSize: SizeConfig.blockSizeHorizontal * 1.05,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
